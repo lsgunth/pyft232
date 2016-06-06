c17a904f85bc40c2bc229b3ad0c3f2ba488a40e3
Logan Gunthorpe <logang@deltatee.com>
2016-06-06 11:08:02 -0600

    BUG: PySerial no longer supplies the FileLike class.
    We change it to use the io.RawIOBase class which matches serial and is
    available in Python 2.6+

--- ft232/libftdi.py
+++ ft232/libftdi.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
 
+import io
 import ctypes as c
 import usb
 import serial
@@ -93,7 +94,7 @@ def list_devices():
                     pass
     return ret
 
-class LibFtdi(serial.FileLike):
+class LibFtdi(io.RawIOBase):
     BAUDRATES = (50,75,110,134,150,200,300,600,1200,1800,2400,4800,9600,
                  19200,38400,57600,115200,230400,460800,500000,576000,921600,
                  1000000,1152000,1500000,2000000,2500000,3000000,3500000,4000000)
